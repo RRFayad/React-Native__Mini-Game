4c7cd67aafcaa559bab3f12bfd8ae4c1af965ef0
RRFayad <renan.r.fayad@gmail.com>
2024-02-06 09:35:51 -0300

    Linear Gradient BG

--- App.jsx
+++ App.jsx
@@ -1,10 +1,16 @@
 import { StyleSheet, View, Text } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { LinearGradient } from "expo-linear-gradient";
+
 import StartGameScreen from "./src/screens/StartGameScreen";
 
 export default function App() {
   return (
     <>
-      <StartGameScreen />
+      <StatusBar style="light" />
+      <LinearGradient colors={["#4e0329", "#ddb52f"]} className="flex-1 ">
+        <StartGameScreen />
+      </LinearGradient>
     </>
   );
 }
--- src/screens/StartGameScreen.jsx
+++ src/screens/StartGameScreen.jsx
@@ -6,7 +6,7 @@ function StartGameScreen() {
   return (
     // inputContainer
     <View
-      className="mx-[24px]  mt-[100px] rounded-lg bg-[#4e0329] p-4 shadow-sm shadow-black"
+      className="mx-[24px]  mt-[100px] rounded-lg bg-[#3b021f] p-4 shadow-sm shadow-black"
       style={{ elevation: 80 }} // Used for the shadow in Android
     >
       <TextInput
@@ -14,8 +14,14 @@ function StartGameScreen() {
         keyboardType="number-pad"
         className="mx-auto my-2 h-[50px] w-[50px] border-b-2 border-[#ddb52f] text-center text-3xl font-bold text-[#ddb52f]"
       />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <View className="mt-4 flex-row justify-around align-middle">
+        <View className="flex-1">
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View className="flex-1">
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
