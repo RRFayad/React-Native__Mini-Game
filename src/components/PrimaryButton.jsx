c8f951369b816be3fe79c1c4ef282cc76ce88123
RRFayad <renan.r.fayad@gmail.com>
2024-02-07 08:23:39 -0300

    Implemented Alert in INput Number

--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -1,13 +1,13 @@
 import { View, Text, Pressable } from "react-native";
 
-function PrimaryButton({ children }) {
+function PrimaryButton({ children, onPress: pressHandler }) {
   return (
     <View className="m-[4px]   ">
       <Pressable
         android_ripple={{ color: "" }}
         className=" rounded-3xl bg-[#72063c] px-[16px] py-[8px] active:bg-[#640233]"
         style={{ elevation: 20 }}
-        onPress={() => console.log("ihaa")}
+        onPress={pressHandler}
       >
         <Text className="text-center text-white">{children}</Text>
       </Pressable>
--- src/screens/StartGameScreen.jsx
+++ src/screens/StartGameScreen.jsx
@@ -1,8 +1,34 @@
-import { TextInput, View, Text } from "react-native";
+import { useState } from "react";
+import { TextInput, View, Text, Alert } from "react-native";
 
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
+  const [enteredNumber, setEnteredNumber] = useState("");
+
+  const numberInputHandler = (enteredText) => {
+    setEnteredNumber(enteredText);
+  };
+
+  const resetInputHandler = (enteredText) => {
+    setEnteredNumber("");
+  };
+
+  const confirmInputHandler = () => {
+    const chosenNumber = parseInt(enteredNumber);
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert("Invalid Value", "Please choose a number between 1 and 99", [
+        {
+          text: "Ok",
+          style: "destructive",
+          onPress: resetInputHandler,
+        },
+      ]);
+      return;
+    }
+    console.log("Ihaa");
+  };
+
   return (
     // inputContainer
     <View
@@ -10,16 +36,18 @@ function StartGameScreen() {
       style={{ elevation: 80 }} // Used for the shadow in Android
     >
       <TextInput
+        className="mx-auto my-2 h-[50px] w-[50px] border-b-2 border-[#ddb52f] text-center text-3xl font-bold text-[#ddb52f]"
         maxLength={2}
         keyboardType="number-pad"
-        className="mx-auto my-2 h-[50px] w-[50px] border-b-2 border-[#ddb52f] text-center text-3xl font-bold text-[#ddb52f]"
+        onChangeText={numberInputHandler}
+        value={enteredNumber}
       />
       <View className="mt-4 flex-row justify-around align-middle">
         <View className="flex-1">
-          <PrimaryButton>Reset</PrimaryButton>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
         </View>
         <View className="flex-1">
-          <PrimaryButton>Confirm</PrimaryButton>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
         </View>
       </View>
     </View>
